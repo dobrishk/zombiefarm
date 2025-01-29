5ce287c5e9a58d011f6c2a9d1807e7472df13516
dobrishk <132073097+dobrishk@users.noreply.github.com>
2025-01-29 16:27:38 +0700

    Update game.py

--- game.py
+++ game.py
@@ -32,9 +32,95 @@ def load_image(name, colorkey=None):
 
 all_sprites = pygame.sprite.Group()
 
+
+class Ruler:
+    def __init__(self, view):
+        self.cell_size = 70
+        if view == 1:
+            self.size = 3
+            self.board = ['white', 'blue', 'red']
+            self.left = 170
+            self.top = 5
+        else:
+            self.size = 5
+            self.board = ['red', 'yellow', 'green', 'blue', 'purple']
+            self.left = 1080
+            self.top = 5
+
+    def render(self, screen):
+        for i in range(self.size):
+            pygame.draw.rect(screen, pygame.Color(self.board[i]),
+                             (self.left + i * self.cell_size, self.top, self.cell_size, self.cell_size), 0)
+
+    def get_cell(self, mouse_pos):
+        x, y = mouse_pos
+        yo, xo = ceil((x - self.left) / self.cell_size), ceil((y - self.top) / self.cell_size)
+        if xo <= 0 or xo > 1 or yo <= 0 or yo > self.size:
+            return None, None
+        return xo, yo
+
+    def on_click(self, mouse_pos):
+        global picked_plant, picked_i
+        x, y = self.get_cell(mouse_pos)
+        if x == None:
+            return
+        x -= 1
+        y -= 1
+        picked_plant = self.board[y]
+        if self.left < ruler_r.left:
+            picked_i = (1, y)
+        else:
+            picked_i = (2, y)
+
+    def get_click(self, mouse_pos):
+        self.on_click(mouse_pos)
+        return self.get_cell(mouse_pos)
+
+    def unpick(self):
+        global picked_plant, picked_i
+        if picked_i[0] == 1:
+            ruler_l.board[picked_i[1]] = picked_plant
+        else:
+            ruler_r.board[picked_i[1]] = picked_plant
+        picked_i = tuple()
+        picked_plant = ''
+
+
+class Cell:
+    def __init__(self):
+        self.disabled = 0
+        self.num = 0
+
+        self.temp_color = 'black'
+
+    def set(self):
+        global picked_plant
+        # if self.disabled == 0 and picked_plant and self.num == 0:
+        #     self.num = picked_plant
+        # picked_plant = 0
+
+        if self.disabled == 0 and picked_plant and self.num == 0:
+            self.num = picked_plant
+            ruler_l.unpick()
+
+        self.disabled = 20
+
+    def draw(self, pos):
+        # col = pygame.Color('black')
+        # if self.num == 1:
+        #     col = pygame.Color('white')
+        # elif self.num == 2:
+        #     col = pygame.Color('orange')
+
+        col = self.temp_color
+
+        pygame.draw.rect(screen, col, pos, 0)
+        self.disabled = max(0, self.disabled - 1)
+
+
 class Field:
     def __init__(self):
-        self.board = [[0] * 18 for i in range(5)] # В будущем здесь будут спрайты клеточек
+        self.board = [[Cell() for i in range(18)] for i in range(5)]
         self.height = 5
         self.width = 18
         self.cell_size = 70
@@ -54,11 +140,10 @@ class Field:
         cs = self.cell_size
         for i in range(5):
             for j in range(18):
-                pygame.draw.rect(screen, pygame.Color('white'), (self.left + j * cs, self.top + i * cs, cs, cs), 1)
+                self.board[i][j].draw((self.left + j * cs, self.top + i * cs, cs, cs))
         for i in range(5):
             for j in range(18):
-                if self.board[i][j]:
-                    pygame.draw.rect(screen, pygame.Color('white'), (self.left + j * cs, self.top + i * cs, cs, cs), 0)
+                pygame.draw.rect(screen, pygame.Color('white'), (self.left + j * cs, self.top + i * cs, cs, cs), 1)
 
     def get_cell(self, mouse_pos):
         x, y = mouse_pos
@@ -69,9 +154,11 @@ class Field:
 
     def on_click(self, mouse_pos):
         x, y = self.get_cell(mouse_pos)
+        if x == None:
+            return
         x -= 1
         y -= 1
-        self.board[x][y] = (self.board[x][y] + 1) % 2
+        self.board[x][y].set()
 
     def get_click(self, mouse_pos):
         self.on_click(mouse_pos)
@@ -82,13 +169,19 @@ clock = pygame.time.Clock()
 fps = 60
 run = True
 field = Field()
+ruler_l = Ruler(1)
+ruler_r = Ruler(2)
 moving = 0
 moving_k = 1
+picked_plant = ''
+picked_i = tuple()
 while run:
     ticks = 0
     screen.fill(pygame.Color('black'))
     if moving:
         field.left += moving_k * 10
+        ruler_l.left += moving_k * 10
+        ruler_r.left += moving_k * 10
         moving -= 10
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -96,10 +189,18 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.unicode == 's':
                 field.move()
+            if event.unicode == 'z':
+                picked_plant = 1
+            if event.unicode == 'x':
+                picked_plant = 2
         if event.type == pygame.MOUSEBUTTONDOWN:
             field.on_click(event.pos)
+            ruler_l.on_click(event.pos)
+            ruler_r.on_click(event.pos)
 
     field.render(screen)
+    ruler_l.render(screen)
+    ruler_r.render(screen)
     all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(fps)
